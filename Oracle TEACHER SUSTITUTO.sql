a2fd3ef124f9076462c659910ed602b850720101
JMDSGitHub <josemendezds@gmail.com>
2025-04-07 18:54:41 +0200

    Lun 07-04 Prof sustituto

--- Oracle TEACHER SUSTITUTO.sql
+++ Oracle TEACHER SUSTITUTO.sql
@@ -128,7 +128,9 @@ uc.search_condition, uc.status
 FROM   user_constraints uc JOIN user_cons_columns ucc 
 ON     uc.table_name = ucc.table_name 
 AND    uc.constraint_name = ucc.constraint_name 
-AND    uc.table_name = UPPER('EMP');  
+AND    uc.table_name = UPPER('EMPLOYEES');  
+
+
 
 
 
